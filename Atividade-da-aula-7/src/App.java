aea78fb9ba2ba519d0a83c39a043161a992143a7
eusimar123 <122410803+eusimar123@users.noreply.github.com>
2023-03-08 17:55:06 -0300

    Atividade aula 7

---
+++ Atividade-da-aula-7/src/App.java
@@ -0,0 +1,167 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        PessoaFisica eusimar = new PessoaFisica("Eusimar", "Queiroz", "Masculino", "Casado", "48705-000", "82187576515", 37);
+        eusimar.setCep("48705-000");
+        System.out.println(eusimar.getCep());
+    }  
+}
+
+class PessoaFisica {
+    private String nome, ultimoNome, genero, estadoCivil, cep ,cpf;
+    private int idade;
+    
+    
+    public PessoaFisica(String nome, String ultimoNome, String genero, String estadoCivil, String cep, String cpf,
+            int idade) {
+        this.nome = nome;
+        this.ultimoNome = ultimoNome;
+        this.genero = genero;
+        this.estadoCivil = estadoCivil;
+        this.cep = cep;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getUltimoNome() {
+        return ultimoNome;
+    }
+
+
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+
+
+    public String getGenero() {
+        return genero;
+    }
+
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep){
+a
+        this.cep = cep;
+        try {
+            if(cep.length() == 8){
+                return;
+            }
+        } catch (Exception e) {
+            cep = "CEP Inválido";
+        }         
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((ultimoNome == null) ? 0 : ultimoNome.hashCode());
+        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+        result = prime * result + ((estadoCivil == null) ? 0 : estadoCivil.hashCode());
+        result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + idade;
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PessoaFisica other = (PessoaFisica) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (ultimoNome == null) {
+            if (other.ultimoNome != null)
+                return false;
+        } else if (!ultimoNome.equals(other.ultimoNome))
+            return false;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (estadoCivil == null) {
+            if (other.estadoCivil != null)
+                return false;
+        } else if (!estadoCivil.equals(other.estadoCivil))
+            return false;
+        if (cep == null) {
+            if (other.cep != null)
+                return false;
+        } else if (!cep.equals(other.cep))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (idade != other.idade)
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "PessoaFisica [nome=" + nome + ", ultimoNome=" + ultimoNome + ", genero=" + genero + ", estadoCivil="
+                + estadoCivil + ", cep=" + cep + ", cpf=" + cpf + ", idade=" + idade + "]";
+    }
+    
+}
