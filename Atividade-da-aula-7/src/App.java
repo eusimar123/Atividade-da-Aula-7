25d7d5fa43a015923dce8daa2ca374fd8634b17e
eusimar123 <122410803+eusimar123@users.noreply.github.com>
2023-03-13 22:01:43 -0300

    Atividade atualizada
    Atividade atualizada

--- Atividade-da-aula-7/src/App.java
+++ Atividade-da-aula-7/src/App.java
@@ -1,117 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        PessoaFisica eusimar = new PessoaFisica("Eusimar", "Queiroz", "Masculino", "Casado", "48705-000", "82187576515", 37);
-        eusimar.setCep("48705-000");
-        System.out.println(eusimar.getCep());
-    }  
+        Person pessoa1 = new Person("Eusimar", "Queiroz", "82187576515", "Casado", "48705000", 'm', 37);
+        pessoa1.setCpf("82187576515");
+        System.out.println(pessoa1.toString());
+    }
 }
 
-class PessoaFisica {
-    private String nome, ultimoNome, genero, estadoCivil, cep ,cpf;
-    private int idade;
+class Person{
+    private String name, lastName, cpf, maritalStatus, cep;
+    private char gender;
+    private int age;
     
-    
-    public PessoaFisica(String nome, String ultimoNome, String genero, String estadoCivil, String cep, String cpf,
-            int idade) {
-        this.nome = nome;
-        this.ultimoNome = ultimoNome;
-        this.genero = genero;
-        this.estadoCivil = estadoCivil;
-        this.cep = cep;
-        this.cpf = cpf;
-        this.idade = idade;
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", lastName=" + lastName + ", cpf=" + cpf + ", maritalStatus=" + maritalStatus
+                + ", cep=" + cep + ", gender=" + gender + ", age=" + age + "]";
     }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
-    public String getUltimoNome() {
-        return ultimoNome;
-    }
-
-
-    public void setUltimoNome(String ultimoNome) {
-        this.ultimoNome = ultimoNome;
-    }
-
-
-    public String getGenero() {
-        return genero;
-    }
-
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-
-    public String getEstadoCivil() {
-        return estadoCivil;
-    }
-
-
-    public void setEstadoCivil(String estadoCivil) {
-        this.estadoCivil = estadoCivil;
-    }
-
-
-    public String getCep() {
-        return cep;
-    }
-    public void setCep(String cep){
-a
-        this.cep = cep;
-        try {
-            if(cep.length() == 8){
-                return;
-            }
-        } catch (Exception e) {
-            cep = "CEP Inválido";
-        }         
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-
-    public int getIdade() {
-        return idade;
-    }
-
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        result = prime * result + ((ultimoNome == null) ? 0 : ultimoNome.hashCode());
-        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
-        result = prime * result + ((estadoCivil == null) ? 0 : estadoCivil.hashCode());
-        result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
         result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
-        result = prime * result + idade;
+        result = prime * result + ((maritalStatus == null) ? 0 : maritalStatus.hashCode());
+        result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+        result = prime * result + gender;
+        result = prime * result + age;
         return result;
     }
-
 
     @Override
     public boolean equals(Object obj) {
@@ -121,47 +39,163 @@ a
             return false;
         if (getClass() != obj.getClass())
             return false;
-        PessoaFisica other = (PessoaFisica) obj;
-        if (nome == null) {
-            if (other.nome != null)
+        Person other = (Person) obj;
+        if (name == null) {
+            if (other.name != null)
                 return false;
-        } else if (!nome.equals(other.nome))
+        } else if (!name.equals(other.name))
             return false;
-        if (ultimoNome == null) {
-            if (other.ultimoNome != null)
+        if (lastName == null) {
+            if (other.lastName != null)
                 return false;
-        } else if (!ultimoNome.equals(other.ultimoNome))
-            return false;
-        if (genero == null) {
-            if (other.genero != null)
-                return false;
-        } else if (!genero.equals(other.genero))
-            return false;
-        if (estadoCivil == null) {
-            if (other.estadoCivil != null)
-                return false;
-        } else if (!estadoCivil.equals(other.estadoCivil))
-            return false;
-        if (cep == null) {
-            if (other.cep != null)
-                return false;
-        } else if (!cep.equals(other.cep))
+        } else if (!lastName.equals(other.lastName))
             return false;
         if (cpf == null) {
             if (other.cpf != null)
                 return false;
         } else if (!cpf.equals(other.cpf))
             return false;
-        if (idade != other.idade)
+        if (maritalStatus == null) {
+            if (other.maritalStatus != null)
+                return false;
+        } else if (!maritalStatus.equals(other.maritalStatus))
+            return false;
+        if (cep == null) {
+            if (other.cep != null)
+                return false;
+        } else if (!cep.equals(other.cep))
+            return false;
+        if (gender != other.gender)
+            return false;
+        if (age != other.age)
             return false;
         return true;
     }
 
+    public Person(String name, String lastName, String cpf, String maritalStatus, String cep, char gender, int age) {
+        try{
+            this.name = name;
+            this.lastName = lastName;
+            this.cpf = cpf;
+            this.maritalStatus = maritalStatus;
+            this.setCep(cep);
+            this.gender = gender;
+            this.age = age;
+        }catch (Exception e){
+            throw new IllegalArgumentException("Verifique as informações inseridas");
+        }
 
-    @Override
-    public String toString() {
-        return "PessoaFisica [nome=" + nome + ", ultimoNome=" + ultimoNome + ", genero=" + genero + ", estadoCivil="
-                + estadoCivil + ", cep=" + cep + ", cpf=" + cpf + ", idade=" + idade + "]";
     }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        // cpfChecker representa o cpf verificador
+        String cpfChecker = cpf.substring(0,9);
+        // multiplierNumber representa o numero a ser multiplicado para cada um dos digitos
+        int multiplierNumber;
+        // resultsTotal é uma variavel para guardar a soma das multiplicações
+        int resultsTotal;
+
+        //inicio loop
+        //loop para navegar os 9 primeiros digitos e multiplicalos pelo multiplier number
+        multiplierNumber = 10;
+        resultsTotal = 0;
+        for (int i = 0; i < cpfChecker.length(); i++) {
+            int myDigit = Character.getNumericValue(cpfChecker.charAt(i));
+            resultsTotal += myDigit * multiplierNumber;
+            multiplierNumber--;
+        }
+        // se o resto de resultsTotal for menor que 2 entao o digito adicionado é 0
+        //senão o digito adicionado é 11 menos o resto de resultsTotal
+        if(resultsTotal%11 < 2 ){
+            cpfChecker = cpfChecker + "0";
+        }else{
+            cpfChecker = cpfChecker + (11 - resultsTotal%11);
+        }
+        //fim loop
+
+        //inicio loop
+        //loop para navegar os 9 primeiros digitos e multiplicalos pelo multiplier number
+        multiplierNumber = 11;
+        resultsTotal = 0;
+        for (int i = 0; i < cpfChecker.length(); i++) {
+            int myDigit = Character.getNumericValue(cpfChecker.charAt(i));
+            resultsTotal += myDigit * multiplierNumber;
+            multiplierNumber--;
+        }
+        // se o resto de resultsTotal for menor que 2 entao o digito adicionado é 0
+        //senão o digito adicionado é 11 menos o resto de resultsTotal
+        if(resultsTotal%11 < 2 ){
+            cpfChecker = cpfChecker + "0";
+        }else{
+            cpfChecker = cpfChecker + (11 - resultsTotal%11);
+        }
+        //fim loop
+
+        //verifica se os cpfs(String) são iguais:
+        if(!(cpf.equals(cpfChecker))){
+            throw new IllegalArgumentException("CPF invalido");
+        }
+
+        // se tudo dar certo, define o cpf do objeto como o cpf passado
+        this.cpf = cpf;
+    }
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        if(cep == null){
+            throw new IllegalArgumentException("CEP Invalido: CEP não pode ser nulo");
+        }
+        
+        int cepSize = cep.length();
+
+        if(cepSize != 8){
+            throw new IllegalArgumentException("CEP Invalido: CEP Incompleto");
+        }
+
+        for (int i = 0; i < cepSize; i++) {
+            char myChar = cep.charAt(i);
+            if(!(myChar >= 48 && myChar <= 57)){
+                throw new IllegalArgumentException("CEP Invalido: CEP somente deve conter numeros");
+            }
+        }
+
+        // se tudo dar certo, define o cep do objeto como o cep passado
+        this.cep = cep;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     
 }
